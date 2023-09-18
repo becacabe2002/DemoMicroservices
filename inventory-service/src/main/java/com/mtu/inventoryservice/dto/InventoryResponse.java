c46799e5c9ee2c74c7b6e7170b385278b1c97c27
Tu Ngo Minh <tungominh.hust@gmail.com>
2023-09-18 23:21:58 +0700

    Change check inventory api

--- inventory-service/src/main/java/com/mtu/inventoryservice/dto/InventoryResponse.java
+++ inventory-service/src/main/java/com/mtu/inventoryservice/dto/InventoryResponse.java
@@ -1,2 +1,15 @@
-package com.mtu.inventoryservice.dto;public class InventoryResponse {
+package com.mtu.inventoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
 }
--- inventory-service/src/main/java/com/mtu/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/mtu/inventoryservice/service/InventoryService.java
@@ -1,10 +1,13 @@
 package com.mtu.inventoryservice.service;
 
+import com.mtu.inventoryservice.dto.InventoryResponse;
 import com.mtu.inventoryservice.repository.InventoryRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,13 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCodes){
+        return inventoryRepository.findBySkuCodeIn(skuCodes).stream()
+                .map(inventory -> {
+                    return InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build();
+                }).toList();
     }
 }
